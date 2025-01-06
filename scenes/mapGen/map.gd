33fd67766881dec3a4a37b2a26b9adad1a097ead
lanmower <almagestfraternite@gmail.com>
2025-01-06 19:49:37 +0200

    open from editor

--- scenes/mapGen/map.gd
+++ scenes/mapGen/map.gd
@@ -21,14 +21,89 @@ func generateMap():
 	generate_terrain()
 
 func generate_terrain():
+	walkable_tiles.clear()
 	for y in range(map_height):
 		for x in range(map_width):
 			var noise_value = noise.get_noise_2d(x, y)
 			var tile_coord = Vector2i()
+			var pos = Vector2i(x, y)
 			if noise_value > 0.03:
 				tile_coord = grassAtlasCoords.pick_random()
-				tile_map.set_cell(Vector2i(x, y), tileset_source, tile_coord, 0)
-				walkable_tiles.append(Vector2i(x,y))
+				tile_map.set_cell(tileset_source, pos, 0, tile_coord)
+				walkable_tiles.append(pos)
 			else:
 				tile_coord = waterCoors.pick_random()
-				tile_map.set_cell(Vector2i(x, y), tileset_source, tile_coord, 0)
+				tile_map.set_cell(tileset_source, pos, 0, tile_coord)
+	
+	connect_islands()
+
+func connect_islands():
+	var islands = find_islands()
+	if islands.size() <= 1:
+		return
+	
+	# Connect all islands to the first one
+	var main_island = islands[0]
+	for i in range(1, islands.size()):
+		connect_two_islands(main_island, islands[i])
+
+func find_islands() -> Array:
+	var islands = []
+	var visited = {}
+	
+	for tile in walkable_tiles:
+		if visited.has(tile):
+			continue
+		
+		var current_island = []
+		var to_visit = [tile]
+		
+		while to_visit.size() > 0:
+			var current = to_visit.pop_front()
+			if visited.has(current):
+				continue
+			
+			visited[current] = true
+			current_island.append(current)
+			
+			# Check neighbors
+			for dx in [-1, 0, 1]:
+				for dy in [-1, 0, 1]:
+					if dx == 0 and dy == 0:
+						continue
+					var neighbor = Vector2i(current.x + dx, current.y + dy)
+					if walkable_tiles.has(neighbor) and not visited.has(neighbor):
+						to_visit.append(neighbor)
+		
+		islands.append(current_island)
+	
+	return islands
+
+func connect_two_islands(island1: Array, island2: Array) -> void:
+	# Find closest points between islands
+	var min_distance = INF
+	var point1 = null
+	var point2 = null
+	
+	for p1 in island1:
+		for p2 in island2:
+			var dist = p1.distance_to(p2)
+			if dist < min_distance:
+				min_distance = dist
+				point1 = p1
+				point2 = p2
+	
+	# Create a path between the closest points
+	var current = point1
+	while current != point2:
+		var dx = sign(point2.x - current.x)
+		var dy = sign(point2.y - current.y)
+		
+		if dx != 0:
+			current = Vector2i(current.x + dx, current.y)
+		elif dy != 0:
+			current = Vector2i(current.x, current.y + dy)
+		
+		if not walkable_tiles.has(current):
+			tile_map.set_cell(tileset_source, current, 0, grassAtlasCoords[0])
+			walkable_tiles.append(current)
